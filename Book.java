fb284b212db99e11ce6b87b755779e1ab426d99e
Kishore <kishore.s.kandasamy@gmail.com>
2026-08-20 19:12:55 +0530

    Book class created

---
+++ Book.java
@@ -0,0 +1,48 @@
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private String status;
+
+    public Book(int id,String title,String author){
+        this.id=id;
+        this.title=title;
+        this.author=author;
+        this.status=true;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title=title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public void setAuthor(String author){
+        this.author=author;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void setStatus(String status){
+        this.status=status;
+    }
+
+
+
+}
