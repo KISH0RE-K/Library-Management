f90d8c9dd41700a74bed2c2d9e79c5419c33e800
Kishore <kishore.s.kandasamy@gmail.com>
2026-08-21 21:31:12 +0530

    Inherited person in member

--- Member.java
+++ Member.java
@@ -1,35 +1,5 @@
-public class Member {
-    private int id;
-    private String name;
-    private String phone;
-
+public class Member extends Person {
     public Member(int id,String name,String phone){
-        this.id=id;
-        this.name=name;
-        this.phone=phone;
-    }
-
-    public int getId(){
-        return id;
-    }
-
-    public void setId(int id){
-        this.id=id;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name){
-        this.name=name;
-    }
-
-    public String getPhone(){
-        return phone;
-    }
-
-    public void setPhone(String phone){
-        this.phone=phone;
+        super(id,name,phone);
     }
 }
---
+++ Person.java
@@ -0,0 +1,35 @@
+public class Person {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Person(int id,String name,String phone){
+        this.id=id;
+        this.name=name;
+        this.phone=phone;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPhone(){
+        return phone;
+    }
+
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void setPhone(String phone){
+        this.phone=phone;
+    }
+}
